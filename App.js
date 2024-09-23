00b42a2b3b7faaece62b4cf635de21f8d79a34fa
Nandish J M <nandish.j.m@ibm.com>
2024-09-23 15:31:25 +0530

    React Basic food order app UI

--- App.js
+++ App.js
@@ -1,24 +1,65 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//Reacct Element
-// const heading = <h1 className="head" tabIndex="3">This is reactJS</h1>
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <img
+          className="logo"
+          src="https://seeklogo.com/images/C/creative-food-company-logo-2DFFD76692-seeklogo.com.png"
+        ></img>
+      </div>
 
-
-//React Functional Component 
-const TitleComponent = () => (
-    <h1>This is Title</h1>
-);
-
-const HeadingComponent = () => (
-    <div className="continer">
-        {/* Component composition:- inside component call another companent   */}
-        <TitleComponent /> 
-        {/*  */}
-        <h1 className="head">This Is Functional React Component</h1>
+      <div className="nav-item">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact US</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
-);
- 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<HeadingComponent />)
+  );
+};
 
+const RestaurantCard = (props) => {
+console.log(props);
+
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHlggjJTT_82lOAdbMOC9A4L9vluWstyhqGw&s"
+      />
+      <h3>Meghana Biriyani</h3>
+      <h4>Biriyani,Andhra,South Indian,North Indian</h4>
+      <h4>4.4 ratings</h4>
+      <h4>30m</h4>
+
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body-continer">
+      <div className="search"> search</div>
+      <div className="res-contanier">
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppComponent = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppComponent />);
