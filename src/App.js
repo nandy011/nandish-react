71ad4e206db2844228c28915cba29209272da713
Nandish J M <nandish.j.m@ibm.com>
2024-09-30 12:23:56 +0530

    React Children Routing using Outlet (Child Routing)

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 
-const AppComponent = () => {
+const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
@@ -19,16 +19,22 @@ const AppComponent = () => {
 const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <AppComponent />,
-    errorElement: <Error />
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+    errorElement: <Error />,
   },
 ]);
 
